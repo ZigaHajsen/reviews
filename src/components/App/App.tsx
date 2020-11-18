803f813916265ad971ac57151a53c12479f30034
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-18 12:48:34 +0100

    Added App component & styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+  Main,
+  Container,
+  Title,
+  TitleText,
+  Underline,
+} from './styles/AppStyle';
+import { Review } from '../';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <Main>
+      <Container>
+        <Title>
+          <TitleText>our reviews</TitleText>
+          <Underline className='underline'></Underline>
+        </Title>
+        <Review />
+      </Container>
+    </Main>
+  );
 };
 
 export default App;
---
+++ src/components/App/styles/AppStyle.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+export const Container = styled.section``;
+
+export const Title = styled.div`
+  text-align: center;
+  margin-bottom: 4rem;
+`;
+
+export const TitleText = styled.h2``;
+
+export const Underline = styled.div`
+  height: 0.25rem;
+  width: 5rem;
+  background: var(--clr-primary-5);
+  margin-left: auto;
+  margin-right: auto;
+`;
